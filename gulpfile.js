8d68a7e44e3fa471430b4f8e748f9b296b5cebfc
Davide Pizzato <davidepizzato@webmapp.it>
2021-05-10 15:49:16 +0200

    Added build ios commands

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ function info(message) {
 function title(message) {
   console.info(
     CONSOLE_COLORS.FgMagenta +
-      "[INFO]    " +
+      "[TITLE]   " +
       CONSOLE_COLORS.Reset +
       " " +
       message
@@ -113,6 +113,13 @@ const argv = yargs(hideBin(process.argv)).options({
     demandOption: true,
     default: "",
     describe: "Set the alias of the android key",
+    type: "string",
+  },
+  b: {
+    alias: "bundle",
+    demandOption: true,
+    default: "1",
+    describe: "Set the bundle version for the ios build",
     type: "string",
   },
 }).argv;
@@ -284,9 +291,8 @@ function update(instanceName) {
     var dir = instancesDir + instanceName,
       url = "";
 
-    if (argv.url) {
-      url = argv.url;
-    } else {
+    if (argv.url) url = argv.url;
+    else {
       url = "https://k.webmapp.it/" + instanceName + "/";
       if (verbose) debug("Using default url: " + url);
     }
@@ -344,7 +350,12 @@ function update(instanceName) {
                 updateIndex(instanceName, configJson.APP.name),
               ];
 
-              Promise.all(promises).then(resolve, reject);
+              Promise.all(promises).then(() => {
+                resolve({
+                  id: configJson.APP.id,
+                  name: configJson.APP.name,
+                });
+              }, reject);
             })
           )
         )
@@ -383,10 +394,14 @@ function updateResources(instanceName, platform) {
   if (platform === "ios" || platform === "android") {
     if (verbose)
       debug("Generating splash screen and icon for platform " + platform);
+
     sh.exec(
       "cordova-res " +
         platform +
-        " --skip-config --copy --icon-foreground-source resources/icon.png --icon-background-source resources/icon.png" +
+        " --skip-config --copy" +
+        (platform === "android"
+          ? " --icon-foreground-source resources/icon.png --icon-background-source resources/icon.png"
+          : "") +
         outputRedirect,
       {
         cwd: instancesDir + instanceName,
@@ -450,39 +465,138 @@ function addAndroidPlatform(instanceName, force) {
   }
 }
 
-function updateAndroidPlatform(instanceName) {
+function updateAndroidPlatform(instanceName, appId, appName) {
   return new Promise((resolve, reject) => {
     runIonicBuild(instanceName);
-    if (!fs.existsSync(instancesDir + instanceName + "/android"))
+    if (!fs.existsSync(instancesDir + instanceName + "/android")) {
+      console.log(
+        "-----------------------------------------------------------------asd"
+      );
       addAndroidPlatform(instanceName);
+    }
 
     if (verbose) debug("Updating android platform");
     sh.exec("npx cap copy android" + outputRedirect, {
       cwd: instancesDir + instanceName,
     });
 
-    if (verbose) debug("Updating android version number");
     var split = version.version.split("."),
       versionCode = "";
     for (var i in split) {
       if (parseInt(split[i]) < 10) versionCode += "0";
       versionCode += split[i];
     }
-    gulp
-      .src(instancesDir + instanceName + "/android/app/build.gradle")
-      .pipe(replace(/versionCode ([0-9]*)/g, "versionCode " + versionCode))
-      .pipe(
-        replace(
-          /versionName "([0-9\.]*)"/g,
-          'versionName "' + version.version + '"'
-        )
-      )
-      .pipe(gulp.dest(instancesDir + instanceName + "/android/app/"))
-      .on("end", () => {
+
+    var promises = [];
+
+    // build.gradle
+    promises.push(
+      new Promise((resolve, reject) => {
+        gulp
+          .src(instancesDir + instanceName + "/android/app/build.gradle")
+          .pipe(replace(/versionCode ([0-9]*)/g, "versionCode " + versionCode))
+          .pipe(
+            replace(
+              /versionName "([0-9\.]*)"/g,
+              'versionName "' + version.version + '"'
+            )
+          )
+          .pipe(gulp.dest(instancesDir + instanceName + "/android/app/"))
+          .on("end", () => {
+            if (verbose) debug("build.gradle updated successfully");
+            resolve();
+          })
+          .on("error", reject);
+      })
+    );
+
+    // AndroidManifest.xml
+    promises.push(
+      new Promise((resolve, reject) => {
+        gulp
+          .src(
+            instancesDir +
+              instanceName +
+              "/android/app/src/main/AndroidManifest.xml"
+          )
+          .pipe(
+            replace(
+              /<manifest ([^>]*) package="([^"]*)"/g,
+              '<manifest $1 package="' + appId + '"'
+            )
+          )
+          .pipe(
+            replace(
+              /android:name="[^"]*.MainActivity"/g,
+              'android:name="' + appId + '.MainActivity"'
+            )
+          )
+          .pipe(
+            gulp.dest(instancesDir + instanceName + "/android/app/src/main/")
+          )
+          .on("end", () => {
+            if (verbose) debug("AndroidManifest.xml updated successfully");
+            resolve();
+          })
+          .on("error", reject);
+      })
+    );
+
+    // strings.xml
+    promises.push(
+      new Promise((resolve, reject) => {
+        gulp
+          .src(
+            instancesDir +
+              instanceName +
+              "/android/app/src/main/res/values/strings.xml"
+          )
+          .pipe(
+            replace(
+              /<string name="app_name">[^<]*<\/string>/g,
+              '<string name="app_name">' + appName + "</string>"
+            )
+          )
+          .pipe(
+            replace(
+              /<string name="title_activity_main">[^<]*<\/string>/g,
+              '<string name="title_activity_main">' + appName + "</string>"
+            )
+          )
+          .pipe(
+            replace(
+              /<string name="package_name">[^<]*<\/string>/g,
+              '<string name="package_name">' + appId + "</string>"
+            )
+          )
+          .pipe(
+            replace(
+              /<string name="custom_url_scheme">[^<]*<\/string>/g,
+              '<string name="custom_url_scheme">' + appId + "</string>"
+            )
+          )
+          .pipe(
+            gulp.dest(
+              instancesDir + instanceName + "/android/app/src/main/res/values/"
+            )
+          )
+          .on("end", () => {
+            if (verbose) debug("strings.xml updated successfully");
+            resolve();
+          })
+          .on("error", reject);
+      })
+    );
+
+    Promise.all(promises).then(
+      (res) => {
         if (verbose) debug("Android platform updated successfully");
         resolve();
-      })
-      .on("error", reject);
+      },
+      (err) => {
+        reject(err);
+      }
+    );
   });
 }
 
@@ -498,18 +612,18 @@ function build(instanceName) {
         if (verbose) debug("`build()` - create completed");
         if (verbose) debug("`build()`- running `update()`");
         update(instanceName, force).then(
-          function () {
+          (result) => {
             if (verbose) debug("`build()` - update completed");
             if (verbose) debug("`build()` completed");
-            resolve();
+            resolve(result);
           },
-          function (err) {
+          (err) => {
             if (verbose) debug("Error running `update()` in `build()`");
             reject(err);
           }
         );
       },
-      function (err) {
+      (err) => {
         if (verbose) debug("Error running `create()` in `build()`");
         reject(err);
       }
@@ -520,11 +634,25 @@ function build(instanceName) {
 function buildAndroid(instanceName) {
   return new Promise((resolve, reject) => {
     build(instanceName).then(
-      () => {
-        initCapacitor(instanceName, "it.webmapp.webmapp", "Webmapp");
-        updateAndroidPlatform(instanceName);
-        updateResources(instanceName, "android");
-        resolve();
+      (result) => {
+        if (!result.id) {
+          abort("The app id could not be found");
+          return;
+        }
+        if (!result.name) {
+          abort("The app name could not be found");
+          return;
+        }
+        initCapacitor(instanceName, result.id, result.name);
+        updateAndroidPlatform(instanceName, result.id, result.name).then(
+          () => {
+            updateResources(instanceName, "android");
+            resolve();
+          },
+          (err) => {
+            reject(err);
+          }
+        );
       },
       (err) => {
         error(err);
@@ -716,6 +844,257 @@ function buildAndroidBundle(instanceName, type) {
   });
 }
 
+function addIosPlatform(instanceName, force) {
+  if (force) {
+    if (verbose) debug("Forcing ios platform installation");
+    sh.exec("rm -rf ios" + outputRedirect, {
+      cwd: instancesDir + instanceName,
+    });
+  }
+  if (!fs.existsSync(instancesDir + instanceName + "/www"))
+    runIonicBuild(instanceName);
+  if (!fs.existsSync(instancesDir + instanceName + "/ios")) {
+    if (verbose) debug("Adding ios platform");
+    sh.exec("npx cap add ios" + outputRedirect, {
+      cwd: instancesDir + instanceName,
+    });
+    if (verbose) debug("Ios platform added successfully");
+  }
+}
+
+function updateIosPlatform(instanceName, appId, appName) {
+  return new Promise((resolve, reject) => {
+    runIonicBuild(instanceName);
+    if (!fs.existsSync(instancesDir + instanceName + "/ios"))
+      addIosPlatform(instanceName);
+
+    if (verbose) debug("Updating ios platform");
+    sh.exec("npx cap copy ios" + outputRedirect, {
+      cwd: instancesDir + instanceName,
+    });
+
+    var promises = [];
+
+    // Info.plist
+    promises.push(
+      new Promise((resolve, reject) => {
+        gulp
+          .src(instancesDir + instanceName + "/ios/App/App/Info.plist")
+          .pipe(
+            replace(
+              /<key>CFBundleDisplayName<\/key>([^<]*)<string>[^<]*<\/string>/g,
+              "<key>CFBundleDisplayName</key>$1<string>" + appName + "</string>"
+            )
+          )
+          .pipe(
+            replace(
+              /<key>CFBundleShortVersionString<\/key>([^<]*)<string>[^<]*<\/string>/g,
+              "<key>CFBundleShortVersionString</key>$1<string>" +
+                version.version +
+                "</string>"
+            )
+          )
+          .pipe(
+            replace(
+              /<key>CFBundleVersion<\/key>([^<]*)<string>[^<]*<\/string>/g,
+              "<key>CFBundleVersion</key>$1<string>" + argv.bundle + "</string>"
+            )
+          )
+          .pipe(gulp.dest(instancesDir + instanceName + "/ios/App/App/"))
+          .on("end", () => {
+            if (verbose) debug("Info.plist updated successfully");
+            resolve();
+          })
+          .on("error", reject);
+      })
+    );
+
+    Promise.all(promises).then(
+      (res) => {
+        if (verbose) debug("Ios platform updated successfully");
+        resolve();
+      },
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+}
+
+function buildIos(instanceName) {
+  return new Promise((resolve, reject) => {
+    build(instanceName).then(
+      (result) => {
+        if (!result.id) {
+          abort("The app id could not be found");
+          return;
+        }
+        if (!result.name) {
+          abort("The app name could not be found");
+          return;
+        }
+        initCapacitor(instanceName, result.id, result.name);
+        updateIosPlatform(instanceName, result.id, result.name).then(
+          () => {
+            updateResources(instanceName, "ios");
+            resolve(result);
+          },
+          (err) => {
+            rejext(err);
+          }
+        );
+      },
+      (err) => {
+        error(err);
+        reject(err);
+      }
+    );
+  });
+}
+
+function createExportOptionsPlist(instanceName, appId, provisioningProfile) {
+  return new Promise((resolve, reject) => {
+    if (!instanceName) {
+      reject("Instance name required. See gulp --help");
+      return;
+    }
+
+    gulp
+      .src("core/exportOptions.plist")
+      .pipe(replace("{{WM_APP_ID}}", appId))
+      .pipe(replace("{{WM_PROVISIONING_PROFILE}}", provisioningProfile))
+      .pipe(gulp.dest("builds/" + instanceName + "/ios/tmp/"))
+      .on("end", () => {
+        if (verbose) debug("exportOptions.plist updated");
+        resolve();
+      })
+      .on("error", reject)
+      .on("data", () => {});
+  });
+}
+
+function fixPodfile(instanceName) {
+  if (fs.existsSync("instances/" + instanceName + "/ios/App/Podfile")) {
+    if (verbose) debug("Adding fix code to podfile");
+    var podfile = fs.readFileSync(
+      "instances/" + instanceName + "/ios/App/Podfile",
+      "utf8"
+    );
+    var check = `post_install do |installer| 
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['EXPANDED_CODE_SIGN_IDENTITY'] = ""
+      config.build_settings['CODE_SIGNING_REQUIRED'] = "NO"
+      config.build_settings['CODE_SIGNING_ALLOWED'] = "NO"
+    end
+  end
+end
+`;
+    if (podfile.indexOf("post_install do |installer|") === -1)
+      podfile = check + "\n" + podfile;
+    fs.writeFileSync("instances/" + instanceName + "/ios/App/Podfile", podfile);
+    if (verbose) debug("Podfile now has the code");
+    if (verbose) debug("Rerunning pod install");
+    sh.exec("pod install" + outputRedirect, {
+      cwd: "instances/" + instanceName + "/ios/App",
+    });
+    if (verbose) debug("Pod reinstalled");
+  }
+}
+
+function buildAndUploadIos(
+  instanceName,
+  appName,
+  appId,
+  useSpecificProvisioningProfile
+) {
+  return new Promise((resolve, reject) => {
+    var provisioningProfile = useSpecificProvisioningProfile
+      ? appId.split(".").pop() + "_distribution"
+      : "webmapp_distribution";
+
+    if (verbose) debug("Cleaning workspace");
+    sh.exec(
+      'xcodebuild -workspace "App.xcworkspace" -scheme App -sdk iphoneos -configuration Release clean' +
+        outputRedirect,
+      {
+        cwd: "instances/" + instanceName + "/ios/App",
+      }
+    );
+    if (verbose) debug("Workspace cleaned");
+    if (verbose) debug("Editing podfile to prevent archive errors");
+    fixPodfile(instanceName);
+    if (verbose) debug("Podfile edited succesfully");
+    if (verbose) debug("Creating workspace archive");
+    sh.exec(
+      'xcodebuild -workspace "App.xcworkspace" -scheme App -sdk iphoneos -configuration Release archive -archivePath "../../../../builds/' +
+        instanceName +
+        "/ios/" +
+        appName +
+        "_" +
+        version.version +
+        '.xcarchive" CODE_SIGN_STYLE="Manual" DEVELOPMENT_TEAM="BSTW6XXE23" PROVISIONING_PROFILE_SPECIFIER="' +
+        provisioningProfile +
+        '" CODE_SIGN_IDENTITY="Apple Distribution: WEBMAPP SRL (BSTW6XXE23)"' +
+        outputRedirect,
+      {
+        cwd: "instances/" + instanceName + "/ios/App",
+      }
+    );
+    if (verbose) debug("Archive created");
+    if (verbose) debug("Exporting the .ipa");
+    createExportOptionsPlist(instanceName, appId, provisioningProfile).then(
+      () => {
+        sh.exec(
+          'xcodebuild -exportArchive -archivePath "./' +
+            appName +
+            "_" +
+            version.version +
+            '.xcarchive" -exportOptionsPlist "./tmp/exportOptions.plist" -exportPath "./tmp/" -allowProvisioningUpdates',
+          {
+            cwd: "builds/" + instanceName + "/ios/",
+          }
+        );
+        if (verbose) debug(".ipa exported");
+        if (verbose) debug("Uploading exported .ipa");
+        sh.exec(
+          'xcrun altool --upload-app --type ios --file "./App.ipa" --apiKey PT5L38QV2W --apiIssuer "69a6de8e-f769-47e3-e053-5b8c7c11a4d1"' +
+            (verbose ? " --verbose" : ""),
+          {
+            cwd: "builds/" + instanceName + "/ios/tmp/",
+          }
+        );
+        info("Upload completed");
+        resolve();
+      },
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+}
+
+function uploadIos(instanceName) {
+  return new Promise((resolve, reject) => {
+    buildIos(instanceName).then(
+      (result) => {
+        buildAndUploadIos(instanceName, result.name, result.id, false).then(
+          () => {
+            resolve();
+          },
+          (err) => {
+            reject();
+          }
+        );
+      },
+      (err) => {
+        error(err);
+        reject(err);
+      }
+    );
+  });
+}
+
 /**
  * Update the configuration in the core for development purposes
  */
@@ -831,6 +1210,9 @@ gulp.task("deploy-android-apk-debug", function (done) {
   );
 });
 
+/**
+ * Build the android apk to use when testing a prerelease version
+ */
 gulp.task("build-android-apk", function (done) {
   var instanceName = argv.instance ? argv.instance : "";
 
@@ -846,6 +1228,9 @@ gulp.task("build-android-apk", function (done) {
   );
 });
 
+/**
+ * Build and deploy the android apk to use when testing a prerelease version
+ */
 gulp.task("deploy-android-apk", function (done) {
   var instanceName = argv.instance ? argv.instance : "";
 
@@ -868,14 +1253,63 @@ gulp.task("deploy-android-apk", function (done) {
   );
 });
 
-gulp.task("release", function (done) {
+/**
+ * Build the ios platform for the given instance
+ */
+gulp.task("build-ios", function (done) {
+  var instanceName = argv.instance ? argv.instance : "";
+
+  if (verbose) debug("Building iOS platform for instance " + instanceName);
+  buildIos(instanceName).then(
+    () => {
+      done();
+    },
+    (err) => {
+      done();
+    }
+  );
+});
+
+/**
+ * Build the ios platform for the given instance
+ */
+gulp.task("upload-ios", function (done) {
   var instanceName = argv.instance ? argv.instance : "";
 
   if (verbose)
-    debug("Building the android release apk for instance " + instanceName);
-  buildSignedApk(instanceName).then(
+    debug(
+      "Building and uploading the iOS version for instance " + instanceName
+    );
+  uploadIos(instanceName).then(
     () => {
       done();
+    },
+    (err) => {
+      done();
+    }
+  );
+});
+
+/**
+ * Perform all the needed operation to release the ios and android version of the specified app
+ */
+gulp.task("release", function (done) {
+  var instanceName = argv.instance ? argv.instance : "";
+
+  title("Building and uploading the iOS version for instance " + instanceName);
+  uploadIos(instanceName).then(
+    () => {
+      success(instanceName + " ios version uploaded successfully");
+      title("Building the android release apk for instance " + instanceName);
+      buildSignedApk(instanceName).then(
+        () => {
+          success(instanceName + " android version built successfully");
+          done();
+        },
+        (err) => {
+          done();
+        }
+      );
     },
     (err) => {
       done();
