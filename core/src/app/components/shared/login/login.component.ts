e6059f5bb6fe6b1e8e09528fb4023f43780e74dc
Davide Pizzato <davidepizzato@webmapp.it>
2021-05-18 16:03:46 +0200

    Added validation to login form

--- core/src/app/components/shared/login/login.component.ts
+++ core/src/app/components/shared/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController, IonInput, ModalController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 import { LanguagesService } from 'src/app/services/languages.service';
@@ -9,18 +10,30 @@ import { LanguagesService } from 'src/app/services/languages.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  @ViewChild('username') usernameField: IonInput;
+  @ViewChild('email') emailField: IonInput;
   @ViewChild('password') passwordField: IonInput;
 
   public showPassword: boolean = false;
   public logging: boolean = false;
+  public isSubmitted: boolean = false;
+  public loginForm: FormGroup;
 
   constructor(
     private _alertController: AlertController,
     private _authService: AuthService,
+    private _formBuilder: FormBuilder,
     private _languagesService: LanguagesService,
     private _modalController: ModalController
-  ) {}
+  ) {
+    this.loginForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  get errorControl() {
+    return this.loginForm.controls;
+  }
 
   ngOnInit() {
     setTimeout(() => {
@@ -29,7 +42,7 @@ export class LoginComponent implements OnInit {
   }
 
   setFocus() {
-    this.usernameField.setFocus();
+    this.emailField.setFocus();
   }
 
   togglePasswordVisibility(): void {
@@ -37,38 +50,41 @@ export class LoginComponent implements OnInit {
   }
 
   login(): void {
-    this.logging = true;
-    this._authService
-      .login(this.usernameField.value + '', this.passwordField.value + '')
-      .then(
-        () => {
-          this.logging = false;
-          this.dismiss();
-        },
-        (loginError) => {
-          console.log(loginError);
-          this.logging = false;
-          this._alertController
-            .create({
-              mode: 'ios',
-              header: this._languagesService.translate('generic.warning'),
-              message: this._languagesService.translate('generic.error'),
-              buttons: [
-                {
-                  text: this._languagesService.translate('generic.ok'),
+    this.isSubmitted = true;
+    if (this.loginForm.valid) {
+      this.logging = true;
+      this._authService
+        .login(this.emailField.value + '', this.passwordField.value + '')
+        .then(
+          () => {
+            this.logging = false;
+            this.dismiss();
+          },
+          (loginError) => {
+            console.log(loginError);
+            this.logging = false;
+            this._alertController
+              .create({
+                mode: 'ios',
+                header: this._languagesService.translate('generic.warning'),
+                message: this._languagesService.translate('generic.error'),
+                buttons: [
+                  {
+                    text: this._languagesService.translate('generic.ok'),
+                  },
+                ],
+              })
+              .then(
+                (alert) => {
+                  alert.present();
                 },
-              ],
-            })
-            .then(
-              (alert) => {
-                alert.present();
-              },
-              (alertError) => {
-                console.warn(alertError);
-              }
-            );
-        }
-      );
+                (alertError) => {
+                  console.warn(alertError);
+                }
+              );
+          }
+        );
+    }
   }
 
   forgotPassword(): void {}
--- core/src/app/components/shared/shared.module.ts
+++ core/src/app/components/shared/shared.module.ts
@@ -4,11 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { ModalHeaderComponent } from './partials/modal-header/modal-header.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, ModalHeaderComponent],
-  imports: [CommonModule, FormsModule, IonicModule, TranslateModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    TranslateModule,
+  ],
   exports: [LoginComponent, ModalHeaderComponent],
 })
 export class SharedModule {}
