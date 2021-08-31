e7f8fec43597255bc06440cad352a996c164c03c
N.Biondi <niccolo@3logic.it>
2021-08-31 18:28:47 +0200

    track card animation

--- core/src/app/pages/map/map-track-card/map-track-card.component.ts
+++ core/src/app/pages/map/map-track-card/map-track-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { CGeojsonLineStringFeature } from 'src/app/classes/features/cgeojson-line-string-feature';
 import { GeohubService } from 'src/app/services/geohub.service';
+import { Animation, AnimationController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'webmapp-map-track-card',
@@ -15,15 +16,35 @@ export class MapTrackCardComponent implements OnInit {
   @Output() close = new EventEmitter();
   @Output() openClick = new EventEmitter();
 
+  @ViewChild('favouriteanimation') favouriteanimation: ElementRef;
+
+  private animation?: Animation;
+
   constructor(
-    private _geohubService: GeohubService
+    private _geohubService: GeohubService,
+    private animationCtrl: AnimationController,
+    private _platform: Platform,
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this._platform.ready();
+    console.log('------- ~ file: map-track-card.component.ts ~ line 29 ~ MapTrackCardComponent ~ ngOnInit ~ this.favouriteanimation', this.favouriteanimation);
 
+    this.animation = this.animationCtrl.create()
+      .duration(1000)
+      .addElement(this.favouriteanimation.nativeElement)
+      .fromTo(
+        'opacity',
+        '1',
+        '0')
+      .fromTo(
+        'transform',
+        'scale(0.5,0.5)',
+        'scale(2,2)')
   }
 
   async setFavourite() {
+    this.animation.play();
     await this._geohubService.setFavouriteTrack(this.track.properties.id, !this.track.properties.isFavourite);
     this.track.properties.isFavourite = !this.track.properties.isFavourite;
   }
@@ -32,7 +53,7 @@ export class MapTrackCardComponent implements OnInit {
     this.close.emit()
   }
 
-  open(){    
+  open() {
     this.openClick.emit()
   }
 
