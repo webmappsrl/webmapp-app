329e30c8c280d958854fe74544fbae9aeed5cda1
N.Biondi <niccolo@3logic.it>
2021-10-19 11:41:33 +0200

    distance in card

--- core/src/app/components/cards/card-big/card-big.component.ts
+++ core/src/app/components/cards/card-big/card-big.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { NavigationOptions } from '@ionic/angular/providers/nav-controller';
 import { GeohubService } from 'src/app/services/geohub.service';
+import { GeolocationService } from 'src/app/services/geolocation.service';
+import { GeoutilsService } from 'src/app/services/geoutils.service';
 import { StatusService } from 'src/app/services/status.service';
 import { IGeojsonFeature, iLocalString } from 'src/app/types/model';
 
@@ -11,16 +13,22 @@ import { IGeojsonFeature, iLocalString } from 'src/app/types/model';
   styleUrls: ['./card-big.component.scss'],
 })
 export class CardBigComponent implements OnInit {
+  @Input('showDistance') showDistance: boolean;
+
   public imageUrl: string;
   public title: iLocalString;
   public where: any;
+
+  public distance: number = 0;
 
   private _item: IGeojsonFeature;
 
   constructor(
     private navCtrl: NavController,
     private _statusService: StatusService,
-    private _geoHubService: GeohubService
+    private _geoHubService: GeohubService,
+    private geolocationService: GeolocationService,
+    private geolocationUtils: GeoutilsService
   ) { }
 
   @Input('item') set item(value: IGeojsonFeature) {
@@ -32,8 +40,19 @@ export class CardBigComponent implements OnInit {
     this._setTaxonomy(value);
   }
 
-  ngOnInit(
-  ) { }
+  async ngOnInit(
+  ) {
+    if (this.showDistance) {
+      const loc = await this.geolocationService.location;
+      const distance = this.geolocationUtils.getDistance(loc.getLatLng(), this.geolocationUtils.getFirstPoint(this._item.geometry.coordinates))
+      if (distance > 10000) {
+        this.distance = Math.round(distance / 1000)
+      }
+      else {
+        this.distance = Math.round(distance / 100) / 10
+      }
+    }
+  }
 
   open() {
     this._statusService.route = this._item;
@@ -50,8 +69,8 @@ export class CardBigComponent implements OnInit {
   private async _setTaxonomy(value: IGeojsonFeature) {
     if (value.properties?.taxonomy?.where && value.properties.taxonomy.where.length) {
       let id = value.properties.taxonomy.where[0];
-       const taxonomy  = await this._geoHubService.getWhereTaxonomy(id);
-       this.where = taxonomy.name;
-      }
+      const taxonomy = await this._geoHubService.getWhereTaxonomy(id);
+      this.where = taxonomy.name;
+    }
   }
 }
