b063ad4f5907750ba713f0ba4a6c67f747a9efbf
bongiu <peppedeka@gmail.com>
2022-08-01 09:35:20 +0200

    remove console.log

--- core/src/app/services/tiles.service.ts
+++ core/src/app/services/tiles.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
-import { SQLite, SQLiteDatabaseConfig } from '@ionic-native/sqlite/ngx';
-import { Platform } from '@ionic/angular';
-import { TILES_DOMAIN } from '../constants/geohub';
-import { StorageService } from './base/storage.service';
+import {SQLite, SQLiteDatabaseConfig} from '@ionic-native/sqlite/ngx';
+
+import {Injectable} from '@angular/core';
+import {Platform} from '@ionic/angular';
+import {StorageService} from './base/storage.service';
+import {TILES_DOMAIN} from '../constants/geohub';
 
 const MINIMUMZOOM = 1;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TilesService {
-
   constructor(
     private storage: StorageService,
     private platform: Platform,
     private _sqlite: SQLite,
-  ) { }
+  ) {}
 
   async getTile(coords: number[], fromCache: boolean): Promise<string> {
     const tileId = coords.join('/');
@@ -30,28 +30,28 @@ export class TilesService {
       result = this.getTileFromWeb(coords);
     }
 
-    return result
+    return result;
   }
 
   public getTileFromWeb(coords: number[]) {
     const tileId = coords.join('/');
-    return (`${TILES_DOMAIN}${tileId}.png`);
+    return `${TILES_DOMAIN}${tileId}.png`;
   }
 
   public getCoordsFromUr(url: string): number[] {
     let coords: [number, number, number] = [0, 0, 0];
-    let tmp: string[] = url.split("/");
+    let tmp: string[] = url.split('/');
 
     coords[0] = parseInt(tmp[tmp.length - 3]);
     coords[1] = parseInt(tmp[tmp.length - 2]);
-    coords[2] = parseInt(tmp[tmp.length - 1].split(".")[0]);
+    coords[2] = parseInt(tmp[tmp.length - 1].split('.')[0]);
 
     return coords;
   }
 
   async getFromMBTilesDownloaded(tileId: string): Promise<string> {
     const mbTileFileName = await this.getMbTileFileName(tileId);
-    console.log("------- ~ TilesService ~ getFromMBTilesDownloaded ~ mbTileFileName", mbTileFileName);
+    // console.log("------- ~ TilesService ~ getFromMBTilesDownloaded ~ mbTileFileName", mbTileFileName);
     if (mbTileFileName) {
       return this.getBase64fromMBtile(tileId, mbTileFileName);
     }
@@ -75,8 +75,7 @@ export class TilesService {
 
   getFatherTileId(tileId): string {
     const coord = tileId.split('/');
-    if (coord[0] as number <= MINIMUMZOOM)
-      return null;
+    if ((coord[0] as number) <= MINIMUMZOOM) return null;
     return `${coord[0]--}/${Math.floor(coord[1] / 2)}/${Math.floor(coord[2] / 2)}`;
   }
 
@@ -87,31 +86,32 @@ export class TilesService {
 
     let coordsString: string[] = tileId.split('/');
     const coords: number[] = [];
-    coordsString.forEach(x => { coords.push(+x) })
-    coords[2] = Math.pow(2, (coords[0] as number)) - (coords[2] as number) - 1;
+    coordsString.forEach(x => {
+      coords.push(+x);
+    });
+    coords[2] = Math.pow(2, coords[0] as number) - (coords[2] as number) - 1;
 
     let config: SQLiteDatabaseConfig = {
       name: mbTileFileName,
-      iosDatabaseLocation: "Library",
+      iosDatabaseLocation: 'Library',
     };
 
     const db = await this._sqlite.create(config);
     const value = await db.executeSql(
-      "SELECT BASE64(tile_data) AS tile FROM images INNER JOIN map ON images.tile_id = map.tile_id WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?;",
-      coords
-    )
+      'SELECT BASE64(tile_data) AS tile FROM images INNER JOIN map ON images.tile_id = map.tile_id WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?;',
+      coords,
+    );
 
     let tileData = value.rows.item(0);
     let base64: string;
 
     if (
-      typeof tileData !== "undefined" &&
-      typeof tileData.tile !== "undefined" &&
-      tileData.tile !== ""
+      typeof tileData !== 'undefined' &&
+      typeof tileData.tile !== 'undefined' &&
+      tileData.tile !== ''
     ) {
-      base64 = "data:image/png;base64, " + tileData.tile;
+      base64 = 'data:image/png;base64, ' + tileData.tile;
     }
     return base64;
-
   }
 }
