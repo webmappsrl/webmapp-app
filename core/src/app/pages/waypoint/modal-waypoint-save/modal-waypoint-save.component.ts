2bc88a92401e3c7382a3b3f136fc67376dad1dbe
bongiu <peppedeka@gmail.com>
2022-11-10 12:18:06 +0100

    wm.app.224.02.6

--- core/src/app/pages/waypoint/modal-waypoint-save/modal-waypoint-save.component.ts
+++ core/src/app/pages/waypoint/modal-waypoint-save/modal-waypoint-save.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ModalController} from '@ionic/angular';
+import {ModalController, LoadingController} from '@ionic/angular';
 import {ModalSuccessComponent} from 'src/app/components/modal-success/modal-success.component';
 import {SaveService} from 'src/app/services/save.service';
 import {ILocation} from 'src/app/types/location';
@@ -14,29 +14,97 @@ import {Md5} from 'ts-md5';
   styleUrls: ['./modal-waypoint-save.component.scss'],
 })
 export class ModalWaypointSaveComponent implements OnInit {
-  public position: ILocation;
-  public displayPosition: ILocation;
-  public title: string;
-  public description: string;
-  public waypointtype = 'waypoint';
-  public photos: any[] = [];
-  public validate = false;
-  public isValidArray: boolean[] = [false, false];
+  private _loading;
 
-  public positionString: string;
+  public description: string;
+  public displayPosition: ILocation;
+  public isValidArray: boolean[] = [false, false];
+  public photos: any[] = [];
+  public position: ILocation;
   public positionCity: string = 'città';
+  public positionString: string;
+  public title: string;
+  public validate = false;
+  public waypointtype = 'waypoint';
 
   constructor(
     private _modalController: ModalController,
     private _photoService: PhotoService,
     private _saveService: SaveService,
+    private _loadingCtrl: LoadingController,
   ) {}
+
+  async addPhotos() {
+    console.log('ADD PHOTO');
+    let library = [];
+    const loading = await this._loadingCtrl.create();
+    console.log('CREATE LOADING');
+    loading.present();
+    console.log('PRESENT LOADING');
+    try {
+      library = await this._photoService.getPhotos();
+    } catch (_) {
+      loading.dismiss();
+    }
+    loading.dismiss();
+    console.log('DISMISS LOADING');
+    library.forEach(async libraryItem => {
+      const libraryItemCopy = Object.assign({selected: false}, libraryItem);
+      const photoData = await this._photoService.getPhotoData(libraryItemCopy.photoURL),
+        md5 = Md5.hashStr(JSON.stringify(photoData));
+      let exists: boolean = false;
+      for (let p of this.photos) {
+        const pData = await this._photoService.getPhotoData(p.photoURL),
+          pictureMd5 = Md5.hashStr(JSON.stringify(pData));
+        if (md5 === pictureMd5) {
+          exists = true;
+          break;
+        }
+      }
+      if (!exists) this.photos.push(libraryItemCopy);
+    });
+  }
+
+  close() {
+    this._modalController.dismiss({
+      dismissed: true,
+    });
+  }
+
+  isValid() {
+    this.validate = true;
+    const allValid = this.isValidArray.reduce((x, curr) => {
+      return curr && x;
+    }, true);
+    return allValid;
+  }
 
   ngOnInit() {
     this.positionString = `${this.position.latitude}, ${this.position.longitude}`;
     setTimeout(() => {
       this.displayPosition = this.position;
     }, 2000);
+  }
+
+  async openModalSuccess(waypoint) {
+    const modaSuccess = await this._modalController.create({
+      component: ModalSuccessComponent,
+      componentProps: {
+        type: ESuccessType.WAYPOINT,
+        waypoint,
+      },
+    });
+    await modaSuccess.present();
+    await modaSuccess.onDidDismiss();
+  }
+
+  remove(image: IPhotoItem) {
+    const i = this.photos.findIndex(
+      x => x.photoURL === image.photoURL || (!!x.key && !!image.key && x.key === image.key),
+    );
+    if (i > -1) {
+      this.photos.splice(i, 1);
+    }
   }
 
   async save() {
@@ -61,61 +129,7 @@ export class ModalWaypointSaveComponent implements OnInit {
     await this.openModalSuccess(waypoint);
   }
 
-  close() {
-    this._modalController.dismiss({
-      dismissed: true,
-    });
-  }
-
-  async openModalSuccess(waypoint) {
-    const modaSuccess = await this._modalController.create({
-      component: ModalSuccessComponent,
-      componentProps: {
-        type: ESuccessType.WAYPOINT,
-        waypoint,
-      },
-    });
-    await modaSuccess.present();
-    await modaSuccess.onDidDismiss();
-  }
-
-  async addPhotos() {
-    const library = await this._photoService.getPhotos();
-    library.forEach(async libraryItem => {
-      const libraryItemCopy = Object.assign({selected: false}, libraryItem);
-      const photoData = await this._photoService.getPhotoData(libraryItemCopy.photoURL),
-        md5 = Md5.hashStr(JSON.stringify(photoData));
-      let exists: boolean = false;
-      for (let p of this.photos) {
-        const pData = await this._photoService.getPhotoData(p.photoURL),
-          pictureMd5 = Md5.hashStr(JSON.stringify(pData));
-        if (md5 === pictureMd5) {
-          exists = true;
-          break;
-        }
-      }
-      if (!exists) this.photos.push(libraryItemCopy);
-    });
-  }
-
-  remove(image: IPhotoItem) {
-    const i = this.photos.findIndex(
-      x => x.photoURL === image.photoURL || (!!x.key && !!image.key && x.key === image.key),
-    );
-    if (i > -1) {
-      this.photos.splice(i, 1);
-    }
-  }
-
   setIsValid(idx: number, isValid: boolean) {
     this.isValidArray[idx] = isValid;
-  }
-
-  isValid() {
-    this.validate = true;
-    const allValid = this.isValidArray.reduce((x, curr) => {
-      return curr && x;
-    }, true);
-    return allValid;
   }
 }
