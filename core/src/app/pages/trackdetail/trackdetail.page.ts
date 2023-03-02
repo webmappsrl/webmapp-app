83d7080e458e25e7c1040801cfb3e1adfdb41465
bongiu <peppedeka@gmail.com>
2023-03-02 08:16:21 +0100

    refactor(tracklist.page): apply best practices

--- core/src/app/pages/trackdetail/trackdetail.page.ts
+++ core/src/app/pages/trackdetail/trackdetail.page.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 import {ActivatedRoute} from '@angular/router';
 import {MenuController, ModalController} from '@ionic/angular';
 import {BehaviorSubject} from 'rxjs';
@@ -10,64 +10,39 @@ import {ITrack} from 'src/app/types/track';
 import {ModalSaveComponent} from '../register/modal-save/modal-save.component';
 
 @Component({
-  selector: 'webmapp-trackdetail',
+  selector: 'wm-trackdetail',
   templateUrl: './trackdetail.page.html',
   styleUrls: ['./trackdetail.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class TrackdetailPage implements OnInit {
-  track$: BehaviorSubject<ITrack | null> = new BehaviorSubject(null);
-
-  public trackTime = {hours: 0, minutes: 0, seconds: 0};
-  public trackDistance: number;
-  public trackSlope: number;
-  public trackAvgSpeed: number;
-  public trackTopSpeed: number;
-
   public photos: IPhotoItem[] = [];
-
   public sliderOptions: any = {
     slidesPerView: 2.5,
   };
+  track$: BehaviorSubject<ITrack | null> = new BehaviorSubject(null);
+  public trackAvgSpeed: number;
+  public trackDistance: number;
+  public trackSlope: number;
+  public trackTime = {hours: 0, minutes: 0, seconds: 0};
+  public trackTopSpeed: number;
 
   constructor(
     private _route: ActivatedRoute,
-    private _menuController: MenuController,
     private _geoUtils: GeoutilsService,
-    private _saveService: SaveService,
-    private _modalController: ModalController,
+    private _saveSvc: SaveService,
+    private _menuCtrl: MenuController,
+    private _modalCtrl: ModalController,
     private _sanitizer: DomSanitizer,
   ) {}
 
-  ngOnInit() {
-    this._route.queryParams.subscribe(async params => {
-      const track = await this._saveService.getTrack(params.track);
-      console.log(
-        '------- ~ file: trackdetail.page.ts ~ line 35 ~ TrackdetailPage ~ this.track',
-        track,
-      );
-      this.trackDistance = this._geoUtils.getLength(track.geojson);
-      this.trackSlope = this._geoUtils.getSlope(track.geojson);
-      this.trackAvgSpeed = this._geoUtils.getAverageSpeed(track.geojson);
-      this.trackTopSpeed = this._geoUtils.getTopSpeed(track.geojson);
-      this.trackTime = GeoutilsService.formatTime(this._geoUtils.getTime(track.geojson));
-      console.log(track);
-      this.track$.next(track);
-    });
-  }
-  sanitize(url: string) {
-    return this._sanitizer.bypassSecurityTrustUrl(url);
-  }
-  menu() {
-    this._menuController.enable(true, 'optionMenu');
-    this._menuController.open('optionMenu');
+  closeMenu(): void {
+    this._menuCtrl.close('optionMenu');
   }
 
-  closeMenu() {
-    this._menuController.close('optionMenu');
-  }
-
-  async edit() {
-    const modal = await this._modalController.create({
+  async edit(): Promise<void> {
+    const modal = await this._modalCtrl.create({
       component: ModalSaveComponent,
       componentProps: {
         track: this.track$.value,
@@ -76,19 +51,10 @@ export class TrackdetailPage implements OnInit {
     });
     await modal.present();
     const res = await modal.onDidDismiss();
-    console.log(
-      '------- ~ file: trackdetail.page.ts ~ line 88 ~ TrackdetailPage ~ edit ~ res',
-      res,
-    );
-
     if (!res.data.dismissed) {
       const track: ITrack = Object.assign(this.track$.value, res.data.trackData);
-
-      await this._saveService.updateTrack(track);
-
+      await this._saveSvc.updateTrack(track);
       this.track$.next(track);
-
-      //await this.openModalSuccess(track);
     }
   }
 
@@ -98,5 +64,27 @@ export class TrackdetailPage implements OnInit {
     } else {
       return photo.datasrc;
     }
+  }
+
+  menu(): void {
+    this._menuCtrl.enable(true, 'optionMenu');
+    this._menuCtrl.open('optionMenu');
+  }
+
+  ngOnInit(): void {
+    this._route.queryParams.subscribe(async params => {
+      const track = await this._saveSvc.getTrack(params.track);
+      this.trackDistance = this._geoUtils.getLength(track.geojson);
+      this.trackSlope = this._geoUtils.getSlope(track.geojson);
+      this.trackAvgSpeed = this._geoUtils.getAverageSpeed(track.geojson);
+      this.trackTopSpeed = this._geoUtils.getTopSpeed(track.geojson);
+      this.trackTime = GeoutilsService.formatTime(this._geoUtils.getTime(track.geojson));
+      console.log(track);
+      this.track$.next(track);
+    });
+  }
+
+  sanitize(url: string): SafeUrl {
+    return this._sanitizer.bypassSecurityTrustUrl(url);
   }
 }
