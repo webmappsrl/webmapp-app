8bfd6df4781a612e3c6404d93d3463efed3649de
bongiu <peppedeka@gmail.com>
2023-03-03 09:40:35 +0100

    fix(waypointpage)

--- core/src/app/pages/waypoint/waypoint.page.ts
+++ core/src/app/pages/waypoint/waypoint.page.ts
@@ -1,10 +1,13 @@
-import {NominatimService} from './../../services/nominatim.service';
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ModalController, NavController} from '@ionic/angular';
+
 import {BehaviorSubject, Subject} from 'rxjs';
-import {distinctUntilChanged, take, takeUntil} from 'rxjs/operators';
+import {distinctUntilChanged, takeUntil} from 'rxjs/operators';
+
 import {GeolocationService} from 'src/app/services/geolocation.service';
+import {NominatimService} from 'src/app/services/nominatim.service';
 import {ILocation} from 'src/app/types/location';
+
 import {ModalWaypointSaveComponent} from './modal-waypoint-save/modal-waypoint-save.component';
 
 @Component({
@@ -13,24 +16,28 @@ import {ModalWaypointSaveComponent} from './modal-waypoint-save/modal-waypoint-s
   styleUrls: ['./waypoint.page.scss'],
 })
 export class WaypointPage implements OnInit, OnDestroy {
-  public position1: string = 'nome città';
-  public position2: string = 'indirizzo';
-  public locationString: string;
-  public location: ILocation;
-  nominatimObj$: BehaviorSubject<any> = new BehaviorSubject(null);
-
   private _destroyer: Subject<boolean> = new Subject<boolean>();
 
+  location: ILocation;
+  locationString: string;
+  nominatimObj$: BehaviorSubject<any> = new BehaviorSubject(null);
+  position1: string = 'nome città';
+  position2: string = 'indirizzo';
+
   constructor(
-    private _geolocationService: GeolocationService,
-    private _modalController: ModalController,
-    private _navController: NavController,
+    private _geolocationSvc: GeolocationService,
     private _nominatimSvc: NominatimService,
+    private _modalCtrl: ModalController,
+    private _navCtrl: NavController,
   ) {}
 
-  ngOnInit() {
-    this._geolocationService.start();
-    this._geolocationService.onLocationChange
+  ngOnDestroy(): void {
+    this._destroyer.next(true);
+  }
+
+  ngOnInit(): void {
+    this._geolocationSvc.start();
+    this._geolocationSvc.onLocationChange
       .pipe(
         distinctUntilChanged((a, b) => a.latitude === b.latitude && a.longitude === b.longitude),
         takeUntil(this._destroyer),
@@ -40,11 +47,7 @@ export class WaypointPage implements OnInit, OnDestroy {
       });
   }
 
-  ngOnDestroy() {
-    this._destroyer.next(true);
-  }
-
-  onChangeLocation(location: ILocation) {
+  onChangeLocation(location: ILocation): void {
     this.locationString = `${location.latitude}, ${location.longitude}`;
     this.location = location;
     this._nominatimSvc
@@ -53,16 +56,16 @@ export class WaypointPage implements OnInit, OnDestroy {
       .subscribe(v => this.nominatimObj$.next(v));
   }
 
-  async save() {
-    const modaSuccess = await this._modalController.create({
+  async save(): Promise<void> {
+    const modaSuccess = await this._modalCtrl.create({
       component: ModalWaypointSaveComponent,
       componentProps: {
         position: this.location,
         nominatim: this.nominatimObj$.value,
       },
     });
-    await modaSuccess.present();
 
-    this._navController.back();
+    await modaSuccess.present();
+    this._navCtrl.back();
   }
 }
