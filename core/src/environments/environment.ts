e4147ae54bbbca2e99f5ac6464a918250f14e243
peppedeka <peppedeka@gmail.com>
2023-09-15 09:09:58 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- core/src/environments/environment.ts
+++ core/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   analyticsId: '285809815',
-  geohubId: 3,
+  geohubId: 29,
   api: 'https://geohub.webmapp.it',
   //api: 'http://127.0.0.1:8000',
 };
