6a060df97401f53b9ee63cd261af0da3733a4aa7
peco11 <alessandropeci@webmapp.it>
2024-08-28 12:03:09 +0200

    chore: Refactor shareTrackByID method in ShareService oc: 3850 (#36)
    * chore: Refactor shareTrackByID method in ShareService oc: 3850
    
    The shareTrackByID method in the ShareService has been refactored to simplify the code and improve readability. Instead of subscribing to an observable and transforming the socialShareText, it now directly shares the track by calling the share method with the trackId as a parameter. This change reduces unnecessary complexity and improves performance.
    
    chore: Update hostToGeohubAppId in ConfigService
    
    The hostToGeohubAppId object in the ConfigService has been updated to use the imported hostToGeohubAppId from 'wm-core/store/api/api.service' instead of the previous hardcoded values. This change allows for more flexibility and easier maintenance of the geohub app IDs based on the hostname.
    
    * chore: Update defaultShareObj text in ShareService
    
    The defaultShareObj text property in the ShareService has been updated to an empty string. This change ensures that the default text for sharing is empty, allowing users to input their own custom text when sharing content.
    
    * chore: Remove empty text property in defaultShareObj
    
    The code change removes the assignment of an empty string to the text property in the defaultShareObj object. This change was made in the ShareService file.

--- core/src/app/services/share.service.ts
+++ core/src/app/services/share.service.ts
@@ -20,22 +20,25 @@ export interface ShareObject {
 export class ShareService {
   private defaultShareObj: ShareObject = {
     title: 'See cool stuff',
-    text: 'Really awesome thing you need to see right meow',
+    text: '',
     url: 'www.webmapp.it',
     dialogTitle: 'Share with buddies',
   };
+  private _host;
+  private _baseLink;
 
   constructor(private _translate: LangService, private _confSvc: ConfService) {
+    this._host = this._confSvc.getHost();
+    this._baseLink = (this._host) ? this._host : `${this._confSvc.geohubAppId}.app.webmapp.it`;
+
     this._translate
       .get([
         'services.share.title',
-        'services.share.text',
         'services.share.url',
         'services.share.dialogTitle',
       ])
       .subscribe(t => {
         this.defaultShareObj.title = t['services.share.title'];
-        this.defaultShareObj.text = t['services.share.text'];
         this.defaultShareObj.url = t['services.share.url'];
         this.defaultShareObj.dialogTitle = t['services.share.dialogTitle'];
       });
@@ -55,26 +58,21 @@ export class ShareService {
     );
   }
 
-  public async shareRoute(route: IGeojsonFeature) {
+  public async shareRoute(route: IGeojsonFeature): Promise<void> {
     return this.share({
       url: `${DEFAULT_ROUTE_LINK_BASEURL}${route.properties.id}`,
     });
   }
 
-  public async shareTrackByID(shareObj: ShareObject) {
-    shareObj = {
-      ...shareObj,
-      ...{url: `${DEFAULT_ROUTE_LINK_BASEURL}${shareObj.id}?app_id=${this._confSvc.geohubAppId}`},
-    };
-    return this.share(shareObj);
+  public shareTrackByID(trackId: number): void {
+    this.share({
+      url: `https://${this._baseLink}/map?track=${trackId}`,
+    });
   }
 
-  public async sharePoiByID(poiId: number){
-    const host = this._confSvc.getHost();
-    const base_link = (host) ? host : `${this._confSvc.geohubAppId}.app.webmapp.it`;
+  public sharePoiByID(poiId: number): void{
     this.share({
-      text: '',
-      url: `https://${base_link}/map?poi=${poiId}`,
+      url: `https://${this._baseLink}/map?poi=${poiId}`,
     })
   }
 }
