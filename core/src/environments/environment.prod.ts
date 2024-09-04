d0caed00aee781d84d05dc87e06ae9597d0afb38
bongiu <peppedeka@gmail.com>
2024-09-04 14:08:08 +0200

    chore: Update environment configuration
    - Added geohubId value of 17 to the production environment
    - Updated api and elasticApi URLs in both production and development environments

--- core/src/environments/environment.prod.ts
+++ core/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  elasticApi: 'https://elastic-json.webmapp.it/v2/search',
+  geohubId: 17,
   api: 'https://geohub.webmapp.it',
+  elasticApi: 'https://elastic-json.webmapp.it/v2/search',
   //api: 'http://127.0.0.1:8000',
   //elasticApi: 'http://localhost:3000/v2/search'
 };
--- core/src/environments/environment.ts
+++ core/src/environments/environment.ts
@@ -5,10 +5,11 @@
 export const environment = {
   production: false,
   analyticsId: '285809815',
-  geohubId: 32,
+  geohubId: 17,
   apiCarg: 'https://carg.maphub.it',
   elasticApi: 'https://elastic-json.webmapp.it/v2/search',
   api: 'https://geohub.webmapp.it',
+  elasticApi: 'https://elastic-json.webmapp.it/v2/search',
   //api: 'http://127.0.0.1:8000',
   //elasticApi: 'http://localhost:3000/v2/search'
 };
