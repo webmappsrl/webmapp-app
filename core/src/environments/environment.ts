013cd07999cf871d72c8bae95732cc49e3efac1d
Alessandro <alessandropeci@webmapp.it>
2024-09-09 11:05:14 +0200

    fix: Remove duplicate elasticApi URL
    The duplicate elasticApi URL was removed from the environment configuration file.

--- core/src/environments/environment.ts
+++ core/src/environments/environment.ts
@@ -9,7 +9,6 @@ export const environment = {
   apiCarg: 'https://carg.maphub.it',
   elasticApi: 'https://elastic-json.webmapp.it/v2/search',
   api: 'https://geohub.webmapp.it',
-  elasticApi: 'https://elastic-json.webmapp.it/v2/search',
   //api: 'http://127.0.0.1:8000',
   //elasticApi: 'http://localhost:3000/v2/search'
 };
