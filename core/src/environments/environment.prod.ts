d281e89afd6f1434654cd94242d8bdaa6d484bbb
bongiu <peppedeka@gmail.com>
2024-09-11 10:04:03 +0200

    chore: Update environment configuration
    - Added AWS API endpoint for geohub
    - Updated production environment with AWS API endpoint

--- core/src/environments/environment.prod.ts
+++ core/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   geohubId: 17,
   api: 'https://geohub.webmapp.it',
   elasticApi: 'https://elastic-json.webmapp.it/v2/search',
+  awsApi: 'https://wmfe.s3.eu-central-1.amazonaws.com/geohub',
   //api: 'http://127.0.0.1:8000',
   //elasticApi: 'http://localhost:3000/v2/search'
 };
--- core/src/environments/environment.ts
+++ core/src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   apiCarg: 'https://carg.maphub.it',
   elasticApi: 'https://elastic-json.webmapp.it/v2/search',
   api: 'https://geohub.webmapp.it',
+  awsApi: 'https://wmfe.s3.eu-central-1.amazonaws.com/geohub',
   //api: 'http://127.0.0.1:8000',
   //elasticApi: 'http://localhost:3000/v2/search'
 };
