ec317a5dc1f861f92cd7161c5a45ab9182281954
bongiu <peppedeka@gmail.com>
2024-11-08 17:20:43 +0100

    chore: Update aggregate_submodule_changelog.sh
    - Add debug logs at the beginning of the script
    - Check for changes in each submodule and append them to the aggregated changelog if there are any
    - Append the content of the aggregated changelog to the main changelog file

--- aggregate_submodule_changelog.sh
+++ aggregate_submodule_changelog.sh
@@ -1,31 +1,30 @@
-#!/bin/bash
+# aggregate_submodule_changelog.sh
+# Aggiungi log di debug all'inizio
+echo "Esecuzione dello script di aggregazione del changelog dei submodules..."
 
-# Nome del file changelog aggregato temporaneo
 AGGREGATED_CHANGELOG="AGGREGATED_CHANGELOG.md"
 CHANGELOG="CHANGELOG.md"
 
-# Pulizia del file temporaneo
 > $AGGREGATED_CHANGELOG
 
 echo "# Aggregated Submodule Changelog" > $AGGREGATED_CHANGELOG
 
-# Elenco dei submodules
 submodules=("core/src/shared/map-core" "core/src/shared/wm-core" "core/src/shared/wm-types")
 
-# Itera sui submodules e raccoglie i commit recenti solo se il submodule è stato aggiornato
 for submodule in "${submodules[@]}"; do
-  # Controlla se il submodule è cambiato nel commit più recente del repository principale
+  echo "Controllo cambiamenti per il submodule: $submodule"
   if git diff HEAD^ HEAD -- "$submodule" | grep -q 'Subproject commit'; then
+    echo "Trovati cambiamenti in $submodule"
     echo -e "\n## Changes in $submodule" >> $AGGREGATED_CHANGELOG
 
-    # Identifica i commit nel submodule dalla revisione precedente alla revisione corrente
     previous_commit=$(git log -1 --pretty=format:"%h" HEAD^ -- "$submodule")
     current_commit=$(git log -1 --pretty=format:"%h" HEAD -- "$submodule")
     (cd $submodule && git log --oneline "$previous_commit".."$current_commit") >> $AGGREGATED_CHANGELOG
+  else
+    echo "Nessun cambiamento trovato in $submodule"
   fi
 done
 
-# Aggiungi il contenuto del changelog aggregato al changelog principale
 if [ -f $CHANGELOG ]; then
   cat $AGGREGATED_CHANGELOG >> $CHANGELOG
 else
