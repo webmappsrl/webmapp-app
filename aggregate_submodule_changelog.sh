811940bd58a094e453a3be49e576a09f8dfc3e4f
bongiu <peppedeka@gmail.com>
2024-11-09 11:56:37 +0100

    chore: Update submodule changelog script
    This commit updates the `aggregate_submodule_changelog.sh` script. It includes changes such as commenting out debug statements and modifying the way the changelog is updated. The script now uses awk to add the `changelog_update` content within the section of the last tag in the CHANGELOG.md file.

--- aggregate_submodule_changelog.sh
+++ aggregate_submodule_changelog.sh
@@ -9,7 +9,7 @@ submodules=("core/src/app/shared/map-core" "core/src/app/shared/wm-core" "core/s
 
 # Stampa una riga vuota per separazione
 echo ""
-echo "Commit che aggiornano i submodules e i relativi messaggi interni del submodule:"
+#echo "Commit che aggiornano i submodules e i relativi messaggi interni del submodule:"
 
 # Variabili per accumulare i messaggi di commit per ogni submodule
 map_core_messages=""
@@ -23,23 +23,23 @@ log_output=$(git log "$last_tag"..HEAD --oneline)
 while IFS= read -r commit_line; do
   commit_hash=$(echo "$commit_line" | awk '{print $1}')
   commit_message=$(echo "$commit_line" | cut -d' ' -f2-)
-  echo "Analizzando commit $commit_hash: $commit_message"  # Debug: Stampa il commit corrente
+  # echo "Analizzando commit $commit_hash: $commit_message"  # Debug: Stampa il commit corrente
 
   for submodule in "${submodules[@]}"; do
     # Controlla se il commit modifica un submodule cercando 'Subproject commit'
     if git show "$commit_hash" -- "$submodule" | grep -q 'Subproject commit'; then
-      echo "Trovato aggiornamento per $submodule nel commit $commit_hash: $commit_message"
+    #  echo "Trovato aggiornamento per $submodule nel commit $commit_hash: $commit_message"
 
       # Ottieni il nuovo hash del commit del submodule con awk
       new_commit=$(git show "$commit_hash" -- "$submodule" | awk '/\+Subproject commit/ {print $3}')
 
       # Verifica se abbiamo un nuovo hash del submodule
       if [ -n "$new_commit" ]; then
-        echo "  Nuovo hash del submodule: $new_commit"  # Debug: Stampa il nuovo hash
+        #  echo "  Nuovo hash del submodule: $new_commit"  # Debug: Stampa il nuovo hash
 
         # Ottieni il messaggio del commit del submodule
         submodule_message=$(git -C "$submodule" log -1 --format="%s" "$new_commit")
-        echo "  Messaggio del commit del submodule ($submodule): $submodule_message"  # Debug: Stampa il messaggio del commit
+     #   echo "  Messaggio del commit del submodule ($submodule): $submodule_message"  # Debug: Stampa il messaggio del commit
 
         # Aggiungi il messaggio del commit alla variabile appropriata per ogni submodule
         case $submodule in
@@ -56,8 +56,8 @@ while IFS= read -r commit_line; do
       else
         echo "  Errore: impossibile trovare il nuovo commit per $submodule nel commit $commit_hash"
       fi
-    else
-      echo "  Nessun aggiornamento per $submodule nel commit $commit_hash"  # Debug: Se non trova un aggiornamento del submodule
+   # else
+    #  echo "  Nessun aggiornamento per $submodule nel commit $commit_hash"  # Debug: Se non trova un aggiornamento del submodule
     fi
   done
 done <<< "$log_output"
@@ -78,23 +78,15 @@ if [ -n "$wm_types_messages" ]; then
 fi
 
 # Stampa per debug cosa conterrà changelog_update
-echo -e "\nContenuto da aggiungere a CHANGELOG.md:\n$changelog_update\n"
+echo "\nContenuto da aggiungere a CHANGELOG.md:\n$changelog_update\n"
 
 # Modifica il changelog solo se ci sono aggiornamenti da aggiungere
 if [ -n "$changelog_update" ]; then
-  temp_file=$(mktemp)
-
-  # Leggi CHANGELOG.md riga per riga
-  while IFS= read -r line; do
-    echo "$line" >> "$temp_file"
-    # Trova l'ultimo tag e aggiungi i nuovi aggiornamenti subito dopo
-    if [[ $line == "## [$last_tag]"* ]]; then
-      echo -e "$changelog_update" >> "$temp_file"
-    fi
-  done < CHANGELOG.md
-
-  # Sovrascrivi CHANGELOG.md con il contenuto aggiornato
-  mv "$temp_file" CHANGELOG.md
+  # Usa awk per aggiungere changelog_update all'interno della sezione dell'ultimo tag
+  awk -v changelog_update="$changelog_update" -v last_tag="## [$last_tag]" '
+    $0 ~ last_tag { print; print changelog_update; next }
+    { print }
+  ' CHANGELOG.md > CHANGELOG.md.tmp && mv CHANGELOG.md.tmp CHANGELOG.md
 
   echo "Aggiornamenti aggiunti a CHANGELOG.md sotto la release $last_tag"
 else
