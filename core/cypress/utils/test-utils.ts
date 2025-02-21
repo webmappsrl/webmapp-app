b78c697d4a7d72f4428d92a757c2ecdf1d2b9213
Alessandro <alessandropeci@webmapp.it>
2025-02-21 10:13:01 +0100

    chore: Add custom Cypress commands and test utilities
    This commit adds a new file `commands.ts` in the `core/cypress/support` directory, which contains custom Cypress commands for login, drag, dismiss, and visit. It also adds a new file `e2e.ts` in the same directory, which imports the `commands.ts` file.
    
    Additionally, this commit introduces a new file `test-utils.ts` in the `core/cypress/utils` directory. This file includes functions for clearing test state, logging into the application, and navigating to different pages.
    
    These changes aim to improve code organization and provide reusable functionality for end-to-end testing.

---
+++ core/cypress/utils/test-utils.ts
@@ -0,0 +1,106 @@
+import {environment} from 'src/environments/environment';
+import {removeAuth} from '@wm-core/utils/localForage';
+
+/**
+ * Clears the test state.
+ * This function uses Cypress to clear cookies and local storage.
+ * It is useful for resetting the application state between end-to-end tests.
+ */
+export function clearTestState(): void {
+  cy.clearLocalStorage();
+  cy.clearCookies();
+  removeAuth();
+}
+
+/**
+ * Logs into the application.
+ * @param email - User's email address.
+ * @param password - User's password.
+ */
+export function e2eLogin(
+  email: string = Cypress.env('email'),
+  password: string = Cypress.env('password'),
+): Cypress.Chainable {
+  const apiLogin = `${environment.api}/api/auth/login`;
+  cy.intercept('POST', apiLogin).as('loginRequest');
+  goProfile();
+  cy.get('.wm-profile-logged-out-login-button').click();
+  cy.get('ion-input[formcontrolname="email"] input').type(email);
+  cy.get('ion-input[formcontrolname="password"] input').type(password);
+  cy.get('.wm-login-submit-button').click();
+  return cy.wait('@loginRequest').its('response.body');
+}
+
+/**
+ * Navigates to the home page.
+ * @param doubleClick if true, performs a double click; otherwise, performs a single click. Default is true.
+ */
+export function goHome(doubleClick = true) {
+  if (doubleClick) {
+    cy.get('#tab-button-home').dblclick();
+  } else {
+    cy.get('#tab-button-home').click();
+  }
+}
+
+/**
+ * Navigates to the map page.
+ */
+export function goMap() {
+  cy.get('#tab-button-map').click();
+}
+
+/**
+ * Navigates to the profile page.
+ */
+export function goProfile() {
+  cy.get('#tab-button-profile').click();
+}
+
+/**
+ * Opens a layer by its title.
+ * @param layerTitle the title of the layer to open.
+ */
+export function openLayer(layerTitle: string) {
+  cy.get('wm-layer-box')
+    .contains('div.wm-box-title', layerTitle)
+    .then($box => {
+      cy.wrap($box).click();
+    });
+}
+
+/**
+ * Opens a POI in a layer by its title.
+ * @param poiTitle the title of the POI to open.
+ */
+export function openPoi(poiTitle: string) {
+  cy.get('wm-poi-box').contains('.wm-box-name', poiTitle).as('poiBox');
+  cy.get('@poiBox').then($poiBox => {
+    cy.wrap($poiBox).click();
+  });
+}
+
+/**
+ * Opens a track in a layer by its title.
+ * @param trackTitle the title of the track to open.
+ */
+export function openTrack(trackTitle: string) {
+  cy.get('wm-search-box').contains('ion-card-title', trackTitle).as('searchBox');
+  cy.get('@searchBox').then($searchBox => {
+    cy.wrap($searchBox).click();
+  });
+}
+
+export const data = {
+  layers: {
+    ecTrack: 'Tracks test e2e',
+    ecPoi: 'Poi test e2e',
+  },
+  tracks: {
+    exampleOne: 'Track example one',
+    exampleTwo: 'Track example two',
+  },
+  pois: {
+    ecPoi: 'Poi example one',
+  },
+};
