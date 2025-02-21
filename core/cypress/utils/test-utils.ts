0cfd854622f1f023726570f129addd4f6928d277
Alessandro <alessandropeci@webmapp.it>
2025-02-21 08:17:40 +0100

    chore: Update test-utils.ts
    - Added `clearTestState` function to clear cookies, local storage, and remove authentication.
    - Added `e2eLogin` function to log into the application using email and password.
    - Added `goProfile` function to navigate to the profile page.
    - Updated `openLayer` function to open a layer by its title.
    - Updated `openPoi` function to open a point of interest by its title.
    - Updated `openTrack` function to open a track by its title.
    - Added `data` object with predefined layer, track, and poi titles for testing purposes.

--- core/cypress/utils/test-utils.ts
+++ core/cypress/utils/test-utils.ts
@@ -1,3 +1,35 @@
+import {environment} from 'src/environments/environment';
+import {removeAuth} from '@wm-core/utils/localForage';
+
+/**
+ * Clears the test state.
+ * This function uses Cypress to clear cookies and local storage.
+ * It is useful for resetting the application state between end-to-end tests.
+ */
+export function clearTestState(): void {
+  cy.clearLocalStorage();
+  cy.clearCookies();
+  removeAuth();
+}
+
+/**
+ * Logs into the application.
+ * @param email - User's email address.
+ * @param password - User's password.
+ */
+export function e2eLogin(
+  email: string = Cypress.env('email'),
+  password: string = Cypress.env('password'),
+): Cypress.Chainable {
+  const apiLogin = `${environment.api}/api/auth/login`;
+  cy.intercept('POST', apiLogin).as('loginRequest');
+  goProfile();
+  cy.get('.wm-profile-logged-out-login-button').click();
+  cy.get('ion-input[formcontrolname="email"] input').type(email);
+  cy.get('ion-input[formcontrolname="password"] input').type(password);
+  cy.get('.wm-login-submit-button').click();
+  return cy.wait('@loginRequest').its('response.body');
+}
 
 /**
  * Navigates to the home page.
@@ -19,13 +51,22 @@ export function goMap() {
 }
 
 /**
+ * Navigates to the profile page.
+ */
+export function goProfile() {
+  cy.get('#tab-button-profile').click();
+}
+
+/**
  * Opens a layer by its title.
  * @param layerTitle the title of the layer to open.
  */
 export function openLayer(layerTitle: string) {
-  cy.get('wm-layer-box').contains('div.wm-box-title', layerTitle).then(($box) => {
-    cy.wrap($box).click();
-  });
+  cy.get('wm-layer-box')
+    .contains('div.wm-box-title', layerTitle)
+    .then($box => {
+      cy.wrap($box).click();
+    });
 }
 
 /**
@@ -34,7 +75,7 @@ export function openLayer(layerTitle: string) {
  */
 export function openPoi(poiTitle: string) {
   cy.get('wm-poi-box').contains('.wm-box-name', poiTitle).as('poiBox');
-  cy.get('@poiBox').then(($poiBox) => {
+  cy.get('@poiBox').then($poiBox => {
     cy.wrap($poiBox).click();
   });
 }
@@ -45,7 +86,21 @@ export function openPoi(poiTitle: string) {
  */
 export function openTrack(trackTitle: string) {
   cy.get('wm-search-box').contains('ion-card-title', trackTitle).as('searchBox');
-  cy.get('@searchBox').then(($searchBox) => {
+  cy.get('@searchBox').then($searchBox => {
     cy.wrap($searchBox).click();
   });
 }
+
+export const data = {
+  layers: {
+    ecTrack: 'Tracks test e2e',
+    ecPoi: 'Poi test e2e',
+  },
+  tracks: {
+    exampleOne: 'Track example one',
+    exampleTwo: 'Track example two',
+  },
+  pois: {
+    exampleOne: 'Poi example one',
+  },
+};
