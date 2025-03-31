940b0e6f66b099ffac72451738c5dbe73187787e
peco11 <alessandropeci@webmapp.it>
2025-03-31 12:23:48 +0200

    chore: update layers view oc:5203 (#110)
    chore: Update map-details component
    
    - Updated the click event for the back button
    - Added border-radius to the map details component
    - Adjusted the margin-top and border-radius for certain elements in the SCSS file
    - Removed unused output events from the component
    - Modified the logic for handling different map details statuses
    
    chore: Update wm-status-filter in map page
    
    - Added event listener for removeLayerEVT to close details
    - Updated setMapDetailsStatus dispatch to backOfMapDetails
    - Added closeDetails method to dispatch backOfMapDetails
    
    Updated submodule core/src/app/shared/wm-core
    
    chore: Update map page HTML and SCSS
    
    - Added conditional class "only-title" to wm-map-details element in map.page.html
    - Updated styles for wm-status-filter element in map.page.scss
    chore: Update map page HTML and SCSS
    
    - Add conditional class "only-title" to wm-map-details element in map.page.html
    - Update styles for wm-status-filter element in map.page.scss

--- core/src/app/pages/map/map-details/map-details.component.ts
+++ core/src/app/pages/map/map-details/map-details.component.ts
@@ -12,7 +12,10 @@ import {Animation, AnimationController, Gesture, GestureController, Platform} fr
 import {Store} from '@ngrx/store';
 import {UrlHandlerService} from '@wm-core/services/url-handler.service';
 import {featureOpened} from '@wm-core/store/features/features.selector';
-import {setMapDetailsStatus} from '@wm-core/store/user-activity/user-activity.action';
+import {
+  backOfMapDetails,
+  setMapDetailsStatus,
+} from '@wm-core/store/user-activity/user-activity.action';
 import {mapDetailsStatus} from '@wm-core/store/user-activity/user-activity.selector';
 import {BehaviorSubject} from 'rxjs/internal/BehaviorSubject';
 
@@ -45,7 +48,6 @@ export class MapDetailsComponent implements AfterViewInit {
     private _animationCtrl: AnimationController,
     private _gestureCtrl: GestureController,
     private _store: Store,
-    private _urlHandlerSvc: UrlHandlerService,
   ) {}
 
   ngAfterViewInit(): void {
@@ -72,10 +74,6 @@ export class MapDetailsComponent implements AfterViewInit {
   background(): void {
     this.setAnimations(`${this._getCurrentHeight()}px`, '0px');
     this.isOpen$.next(false);
-  }
-
-  clickBack(): void {
-    this._store.dispatch(setMapDetailsStatus({status: 'none'}));
   }
 
   full(): void {
