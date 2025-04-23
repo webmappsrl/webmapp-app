6aed8bf22647499f55c46d03e17a8cd62060e055
bongiu <peppedeka@gmail.com>
2025-04-23 12:54:31 +0200

    ....

--- core/src/environments/environment.ts
+++ core/src/environments/environment.ts
@@ -6,7 +6,7 @@ import {Environment} from '@wm-types/environment';
 
 export const environment: Environment = {
   production: false,
-  appId: 3,
+  appId: 33,
   shardName: 'geohub',
   shards: {
     geohub: {
@@ -23,7 +23,7 @@ export const environment: Environment = {
     },
     camminiditalia: {
       origin: 'https://camminiditalia.maphub.it',
-      elasticApi: 'https://elastic-json.webmapp.it/v2/search',
+      elasticApi: 'https://camminiditalia.maphub.it/api/v2/elasticsearch',
       graphhopperHost: 'https://graphhopper.webmapp.it/',
       awsApi: 'https://wmfe.s3.eu-central-1.amazonaws.com/camminiditalia',
     },
