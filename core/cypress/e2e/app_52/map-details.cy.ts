958733c0f1aeaaf512102abd496f0e610e2269de
Alessandro <alessandropeci@webmapp.it>
2025-05-12 11:19:39 +0200

    ...

--- core/cypress/e2e/app_52/map-details.cy.ts
+++ core/cypress/e2e/app_52/map-details.cy.ts
@@ -1,6 +1,6 @@
 import {clearTestState, goMap} from "cypress/utils/test-utils";
 
-const waitTime = 700;
+const waitTime = 800;
 const zoom_coordinates = [8.229458808898698, 280.25369262695403];
 const track_coordinates = [208.15484619140534, 412.5570373535229];
 const empty_click_coordinates = [141.97242736816406, 294.8253173828707];
@@ -14,7 +14,7 @@ describe('Open and close Map Details [oc:5500] [https://orchestrator.maphub.it/r
 
   it('should open Map Details on click track in map', () => {
     goMap();
-    cy.wait(waitTime);
+    cy.wait(1500);
     cy.get('.ol-viewport .ol-layer canvas').click(zoom_coordinates[0], zoom_coordinates[1]);
     cy.wait(waitTime);
     cy.get('.ol-viewport .ol-layer canvas').click(track_coordinates[0], track_coordinates[1]);
