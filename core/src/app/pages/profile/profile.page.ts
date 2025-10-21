26b338d78a5430721833458db5aee475b66a6a3c
peppedeka <peppedeka@gmail.com>
2025-10-21 16:47:29 +0200

    refactor(profile): ♻️ remove unused confAPP selector
    - Removed the import and usage of the `confAPP` selector from the profile page component.
    - Cleaned up associated imports to streamline the codebase and reduce unnecessary dependencies.
    - Ensured the component functionality remains unaffected by this change.

--- core/src/app/pages/profile/profile.page.ts
+++ core/src/app/pages/profile/profile.page.ts
@@ -3,7 +3,7 @@ import {ModalController} from '@ionic/angular';
 import {Observable, Subject, Subscription} from 'rxjs';
 import {SettingsComponent} from 'src/app/components/settings/settings.component';
 import {select, Store} from '@ngrx/store';
-import {confAUTHEnable, confMAPHitMapUrl, confAPP} from '@wm-core/store/conf/conf.selector';
+import {confAUTHEnable, confMAPHitMapUrl} from '@wm-core/store/conf/conf.selector';
 import {isLogged} from '@wm-core/store/auth/auth.selectors';
 import {Router} from '@angular/router';
 import {filter, skip, take} from 'rxjs/operators';
@@ -15,7 +15,6 @@ import {WmFeature} from '@wm-types/feature';
 import {MultiPolygon} from 'geojson';
 import {getHitmapFeatures} from '@map-core/utils';
 import {WmHomeHitMapComponent} from '@wm-core/home/home-hitmap/home-hitmap.component';
-import {IAPP} from '@wm-core/types/config';
 
 @Component({
   selector: 'webmapp-page-profile',
@@ -30,7 +29,6 @@ export class ProfilePage implements OnDestroy {
   authEnable$: Observable<boolean> = this._store.select(confAUTHEnable);
   isLogged$: Observable<boolean> = this._store.pipe(select(isLogged));
   confMAPHitMapUrl$: Observable<string | null> = this._store.select(confMAPHitMapUrl);
-  confAPP$: Observable<IAPP> = this._store.select(confAPP);
   hitmapFeatures$: Observable<WmFeature<MultiPolygon>[]> = from(getHitmapFeatures());
   @ViewChild(WmHomeHitMapComponent) hitmapComp: WmHomeHitMapComponent;
 
